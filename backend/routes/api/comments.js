746f5809fab5b435dc04acd418ccc3440c987657
Shem <smagnezi8@gmail.com>
2024-02-15 12:20:09 +0000

    Add routes for getting all comments and deleting a comment by ID

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,5 +1,44 @@
+/**
+ * Express router for handling comments API.
+ * @module routes/api/comments
+ */
+
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Comment = mongoose.model("Comment");
+
+/**
+ * Route for getting all comments.
+ * @name GET /api/comments
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - JSON response containing all comments.
+ */
+router.get("/", async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+/**
+ * Route for deleting a comment by ID.
+ * @name DELETE /api/comments/:id
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - JSON response indicating successful deletion.
+ */
+router.delete("/:id", (req, res) => {
+    Comment.findByIdAndRemove(req.params.id)
+        .then(() => {
+        res.json({ });
+        })
+        .catch((err) => console.log(err));
+    });
 
 module.exports = router;
