564ecd6134af2bd546b33b76a652bde369bac9c4
Shem <smagnezi8@gmail.com>
2025-04-10 11:37:28 +0300

    Add newline in App.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import RouterList from './components/RouterList';
 
+
 const App: React.FC = () => {
   return (
     <Router>
