5208ec1c268a8d8bb5d04d1065a81f0e9d1494d3
WilcoApp <hello@wilco.gg>
2025-05-19 11:23:34 +0000

    Initial commit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,24 +1,18 @@
+#!/bin/bash
+
 WILCO_ID="`cat .wilco`"
 CODESPACE_BACKEND_HOST=$(curl -s "${ENGINE_BASE_URL}/api/v1/codespace/backendHost?codespaceName=${CODESPACE_NAME}&portForwarding=${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" | jq -r '.codespaceBackendHost')
 CODESPACE_BACKEND_URL="https://${CODESPACE_BACKEND_HOST}"
 export ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 
+# Install Amazon Q CLI
+echo "Installing Amazon Q CLI..."
+export AMAZON_Q_VERSION=latest && \
+    curl --proto '=https' --tlsv1.2 -sSf "https://desktop-release.q.us-east-1.amazonaws.com/${AMAZON_Q_VERSION}/amazon-q.deb" -o amazon-q.deb && \
+    sudo apt-get update && \
+    sudo apt-get install -y ./amazon-q.deb && \
+    rm ./amazon-q.deb && \
+    sudo rm -rf /var/lib/apt/lists/*
+
 # Update engine that codespace started for user
 curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
-
-# Export backend envs when in codespaces
-echo "export CODESPACE_BACKEND_HOST=\"${CODESPACE_BACKEND_HOST}\"" >> ~/.bashrc
-echo "export CODESPACE_BACKEND_URL=\"${CODESPACE_BACKEND_URL}\"" >> ~/.bashrc
-echo "export CODESPACE_WDS_SOCKET_PORT=443" >> ~/.bashrc
-
-# Export welcome prompt in bash:
-echo "printf \"\n\n☁️☁️☁️️ Anythink: Develop in the Cloud ☁️☁️☁️\n\"" >> ~/.bashrc
-echo "printf \"\n\x1b[31m \x1b[1m👉 Type: \\\`docker compose up\\\` to run the project. 👈\n\n\"" >> ~/.bashrc
-
-nohup bash -c "cd /wilco-agent && node agent.js &" >> /tmp/agent.log 2>&1
-
-# Check if docker is installed
-if command -v docker &> /dev/null
-then
-    docker compose pull
-fi
