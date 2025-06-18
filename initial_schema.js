4a88d17e8195351eeb6a7f7d9f22b3d1bf4aa75f
WilcoApp <hello@wilco.gg>
2025-06-18 09:49:06 +0000

    Initial commit

--- initial_schema.js
+++
@@ -1,9 +0,0 @@
-// This is the initial schema template that needs to be completed
-const initial_schema = {
-  // TODO: Add the $jsonSchema operator here
-  bsonType: "object",
-  required: [], // TODO: Add review_id as required
-  properties: {
-    // TODO: Add review_id field with string type
-  },
-};
