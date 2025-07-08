33db17b0f57c530cd6bc31192a99844d4003f883
WilcoApp <hello@wilco.gg>
2025-07-08 13:25:34 +0000

    Initial commit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 WILCO_ID="`cat .wilco`"
 CODESPACE_BACKEND_HOST=$(curl -s "${ENGINE_BASE_URL}/api/v1/codespace/backendHost?codespaceName=${CODESPACE_NAME}&portForwarding=${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" | jq -r '.codespaceBackendHost')
 CODESPACE_BACKEND_URL="https://${CODESPACE_BACKEND_HOST}"
@@ -7,3 +5,5 @@ export ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 
 # Update engine that codespace started for user
 curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
+
+pip install --user -r requirements.txt
